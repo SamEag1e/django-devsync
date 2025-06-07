8dcef7aa94dfa81dc90d01b03f89ccf0ad323387
SamEagle <samadeagle@yahoo.com>
2025-06-07 14:03:58 +0330

    Update get_settings

--- src/django_devsync/get_settings.py
+++ src/django_devsync/get_settings.py
@@ -1,16 +1,22 @@
 import os
+import importlib
 
 
 def get_settings():
-    # Try to guess settings module or ask user
     settings_module = os.environ.get("DJANGO_SETTINGS_MODULE")
-    if not settings_module:
-        # Ask for entering settings module,
-        # and check if it exists and it's the right one for django project
-        # if not, ask again (in a loop)
+
+    if settings_module:
+        return settings_module
+
+    while True:
         settings_module = input(
             "Enter your DJANGO_SETTINGS_MODULE "
             "(e.g. myproject.settings, src.config.settings, etc): "
         ).strip()
 
-    return settings_module
+        try:
+            importlib.import_module(settings_module)
+            return settings_module  # Returns once a valid module is found
+
+        except ModuleNotFoundError:
+            print(f"Invalid settings module: '{settings_module}'. Try again.")
